14e9d2fab38b3b2bd04203d7e5baf3521705abf6
zsm <782683353@qq.com>
2025-01-19 15:04:41 +0800

    feat: 添加 sitemap 和 robots.txt 生成功能

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://recordingtotextconverter.it.com',
+      dynamicRoutes: [
+        '/',
+        '/recorder',
+        '/faq',
+        '/blog',
+        '/blog/1'  // 添加已知的博客文章路由
+      ],
+      exclude: ['/404'],
+      lastmod: new Date(),
+      robots: [
+        {
+          userAgent: '*',
+          allow: '/',
+          disallow: ['/admin', '/private']
+        }
+      ],
+      outDir: 'dist'
+    })
+  ],
   build: {
     outDir: 'dist',
     sourcemap: true,
